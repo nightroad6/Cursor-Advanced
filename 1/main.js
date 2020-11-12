95da2cbd31347e28dad42e740686088b01c67471
Anastasia <anastasianightroad@gmail.com>
2020-11-12 18:33:19 +0200

    done

--- 1/main.js
+++ 1/main.js
@@ -4,33 +4,24 @@ let lily = 90.2345;
 let fund = 500;
 
 const maxPrice = Math.max(sunflower, rose, lily);
-const maxPriceName = "Максимальне число";
 const minPrice = Math.min(sunflower, rose, lily)
-const minPriceName = "Мінімальне число";
 const sum = sunflower+rose+lily; 
-const sumName = "Сума";
 const coins = Math.round(sunflower)+Math.round(rose)+Math.round(lily);
-const coinsFloorName = "Сума з округленням в меншу сторону";
 const coinsFloor = Math.floor(coins)
-const coinsName = "Сума без копійок";
 const sumCeil = Math.ceil(coins/100)*100;
-const sumCeilName = "Сума округлена до сотень";
 const balance = fund - sum;
 const avarageValue = sum/3;
 const avarageValueCeil = Math.ceil(avarageValue*100)/100;
-function getRandom (min, max) {
-    let rand = min - 0.5 + Math.random() * (max - min + 1);
-    return Math.round(rand);
-}
-const sumWithSale = Math.ceil(sum-(sum/100*(getRandom(1, 100))));
-// const profit = sum/2-()
+let randSale = (Math.random() * (99 - 1) + 1).toFixed(0);
+const priceWithSale = sum.toFixed(2) - (sum.toFixed(2) / 100 * randSale);
+const sallary = ((sum / 2)- (sum / 100 * randSale)).toFixed(2);
 
-
-console.log (maxPriceName, maxPrice);
-console.log (minPriceName, minPrice);
-console.log (sumName, sum)
-console.log (coinsName, coins, coinsFloorName, coinsFloor);
-console.log (sumCeilName, sumCeil);
+console.log ("Максимальне число", maxPrice);
+console.log ("Мінімальне число", minPrice);
+console.log ("Сума", sum)
+console.log ("Сума з округленням в меншу сторону", coinsFloor);
+console.log ("Сума без копійок", coins);
+console.log ("Сума округлена до сотень", sumCeil);
 
 function par(coinsFloor){
     if(coinsFloor%2 == 0){
@@ -42,9 +33,9 @@ function par(coinsFloor){
  par(coinsFloor);
 
  console.log("Решта",balance)
- console.log ("Середне значення", avarageValueCeil)
- console.log("Ваша знижка",getRandom(1, 100)+"%");
- console.log("Сума зі знижкою", sumWithSale)
-//  console.log("", )
- 
+ console.log("Середне значення", avarageValueCeil)
+ console.log("Знижка", randSale+"%")
+ console.log("Сума зі знижкою", priceWithSale.toFixed(2));
+ console.log("Прибуток",sallary)
+
  
