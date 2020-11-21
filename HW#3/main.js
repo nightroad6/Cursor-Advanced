5453a4fb72bb84e9d64c271c2283dfed77be7cbd
Anastasia <anastasianightroad@gmail.com>
2020-11-22 00:56:01 +0200

    3-6 tasks done in hw-3

--- HW#3/main.js
+++ HW#3/main.js
@@ -32,3 +32,34 @@
 //     return nameType[0].toUpperCase() + nameType.slice(1)
 // }
 // console.log(nameToUpperCase (nameType))
+
+//4-TH TASK
+// const sallaryType = +prompt("Type your sallary");
+// const tax = 19.5;
+// function mathSallaryWithTax (sallaryType, tax) {
+//     let result = sallaryType-(sallaryType/100*tax);
+//     return result
+// }
+// console.log(mathSallaryWithTax(sallaryType, tax));
+
+//5-TH TASK 
+// const minNumber = +prompt("type min number");
+// const maxNumber = +prompt("type max number");
+// function getRandomNumber (minNumber, maxNumber) {
+//     return Math.floor(Math.random()*(maxNumber - minNumber +1)) +minNumber;
+// }
+// alert(getRandomNumber (minNumber, maxNumber))
+
+//6-TH TASK 
+// const word = prompt("Type word");
+// const letter = prompt("Type letter");
+// function countLetter (word, letter) {
+//     let letterCount = 0;
+//     for (let position = 0; position < word.length; position++) {
+//         if (word.charAt(position) == letter) {
+//             letterCount += 1;
+//         }
+//     }
+//     return letterCount;
+// }
+// alert(countLetter (word, letter));
