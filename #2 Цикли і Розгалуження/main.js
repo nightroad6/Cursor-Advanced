047f69192dbb0aeaced97279a6490833405f00e1
Anastasia <anastasianightroad@gmail.com>
2020-11-21 20:46:12 +0200

    fixed all in hw-2

--- #2 Цикли і Розгалуження/main.js
+++ #2 Цикли і Розгалуження/main.js
@@ -1,34 +1,21 @@
-let numberFirst = +prompt("Type number");
-if (numberFirst %2 ==0) {
-    console.log("False");
-} else {
-    console.log("True");
-}
+let numberFirst
+let numberSecond
 
-let numberSecond = +prompt("Type anoter number");
-if (numberSecond %2 ==0) {
-    console.log("False");
-} else {
-    console.log("True");
-}
+do {
+    numberFirst = Number(prompt("Type integer number"));
+} while (!Number.isInteger(numberFirst));
+
+do {
+    numberSecond = Number(prompt("Type integer number"));
+} while (!Number.isInteger(numberSecond));
 
 const answerChosen = confirm("Skip primes?") // primes - парні числа
-console.log(answerChosen)
 
 let result = 0;
-if (!answerChosen) {
-    for (numberFirst; numberFirst <= numberSecond; numberFirst++) {
-        result+= numberFirst;
+for (let i = numberFirst; i <= numberSecond; i++) {
+    if (answerChosen && i % 2 === 0) {
+        continue;
     }
-    alert(result)
-} else {
-    for (numberFirst; numberFirst <= numberSecond; numberFirst++) {
-        if (numberFirst % 2 == 0) {
-            continue;
-        }
-        else {
-            result += numberFirst;
-        }
-    }
-    alert(result)
+    result = result + i;
 }
+alert(result)
