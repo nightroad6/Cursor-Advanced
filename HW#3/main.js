cc9a724e4cef873edcedd237c9d4831549d4c27a
Anastasia <anastasianightroad@gmail.com>
2020-11-21 23:32:40 +0200

    1-3 tasks done

--- HW#3/main.js
+++ HW#3/main.js
@@ -1,6 +1,34 @@
-let typeForm = prompt("Type");
-let typeFormString = typeForm.split("");
+// 1-ST TASK
+// const numberType = +(document.getElementById("Please, type number"));
+// const getMaxDigit = number => {
+//     const splitNumber = number.toString().split('');
+//     let maxDigit = 0;
+//     splitNumber.forEach(digit => {
+//         if(+digit > maxDigit){
+//             maxDigit = digit;
+//         }
+//     });
+//     return maxDigit;
+// }
+// document.writeln(getMaxDigit(numberType));
 
-let maxNumber = Math.max(typeFormString)
-console.log(typeForm);
+// 2-ND TASK
+// const numberType2 = Number(prompt("Type number"));
+// const numberPower = Number(prompt("Type exponent"))
 
+// function toPow(numberType2, numberPower) {
+//     let result = 1;
+//     for(let i = 0; i < numberPower; i++) {
+//         result *= numberType2;
+//     }
+//     return result
+// }
+// console.log(toPow(numberType2, numberPower));
+
+//3-RD TASK
+// let nameType = prompt("Type your name");
+// function nameToUpperCase (strType) {
+//     if(!nameType) return nameType;
+//     return nameType[0].toUpperCase() + nameType.slice(1)
+// }
+// console.log(nameToUpperCase (nameType))
